5ca97eb324cd20021ddf2910bbc3c23c790a1804
Ivan Shkarlet <ivanshkarlet@gmail.com>
2026-03-11 20:09:40 +0300

    2. Добавлены новые классы: Event, EventManager

--- EventManagement/Event.cs
+++ EventManagement/Event.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EventManagement
 {
-    internal class Event
+    public class Event
     {
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public bool ReminderSet { get; set; }
+
+        public Event(string name, DateTime startTime, DateTime endTime, string location, string description)
+        {
+            Name = name; StartTime = startTime; EndTime = endTime;
+            Location = location; Description = description;
+            ReminderSet = false;
+        }
+
+        public void SetReminder()
+        {
+            ReminderSet = true;
+            MessageBox.Show("Напоминание установлено.");
+        }
+
+        public void RemoveReminder()
+        {
+            ReminderSet = false;
+            MessageBox.Show("Напоминание снято.");
+        }
+
+        public override string ToString()
+        {
+            return $"Событие: {Name}\nВремя: {StartTime:dd.MM.yyyy HH:mm} - {EndTime:dd.MM.yyyy HH:mm}\nМесто: {Location}\nОписание: {Description}\nНапоминание: {(ReminderSet ? "Да" : "Нет")}";
+        }
     }
 }
